f939e5947e2cb7f6c083eda027ea85af2ada150f
Josias Junior Santos <josiajrsantos@gmail.com>
2025-11-06 15:56:11 -0300

    Criação do projeto

---
+++ src/main/java/com/senai/Josias/Wesia/Sistema/de/Gestao/de/Estoque/e/Caixa/SistemaDeGestaoDeEstoqueECaixaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.Josias.Wesia.Sistema.de.Gestao.de.Estoque.e.Caixa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestaoDeEstoqueECaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestaoDeEstoqueECaixaApplication.class, args);
+	}
+
+}
